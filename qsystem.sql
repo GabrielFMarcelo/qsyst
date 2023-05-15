32940ec5b323eb73d5a830431c82305e7aa2ff0f
GabrielFMarcelo <124745233+GabrielFMarcelo@users.noreply.github.com>
2023-05-15 15:59:10 +0800

    new sql

--- qsystem.sql
+++ qsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: May 14, 2023 at 05:40 AM
+-- Generation Time: May 15, 2023 at 07:57 AM
 -- Server version: 10.10.2-MariaDB
 -- PHP Version: 7.4.33
 
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_display`
+--
+
+DROP TABLE IF EXISTS `tbl_display`;
+CREATE TABLE IF NOT EXISTS `tbl_display` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `qn` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_transaction`
 --
 
@@ -34,7 +47,15 @@ CREATE TABLE IF NOT EXISTS `tbl_transaction` (
   `tn` varchar(255) DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbl_transaction`
+--
+
+INSERT INTO `tbl_transaction` (`id`, `name`, `tn`, `type`) VALUES
+(1, 'aasdas', 'W569312', 'Withdraw'),
+(2, 'heidel', 'W172686', 'Withdraw');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
